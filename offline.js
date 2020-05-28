7fb427ac73766b22c8e72cbfd42878ef23371e56
KonyshAndrey <66051131+KonyshAndrey@users.noreply.github.com>
2020-05-28 21:48:06 +0500

    Файлы для сайта

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1590667820,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sky-sheet0.png",
+		"images/base-sheet0.png",
+		"images/msg-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/bot-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
